7deeae77736dfb84e70ce07359905a64c39c4f34
Xavi Soler <xavisolba@gmail.com>
2019-04-10 17:03:35 +0200

    simplify node+channel relationship properties

--- neo4j/channels.go
+++ neo4j/channels.go
@@ -15,24 +15,14 @@ const (
 		Capacity: {capacity}
 	} )`
 
-	relChannelNode1Query = `MATCH (n:Node),(c:Channel)
-	WHERE n.PubKey = {node1Pub} AND c.ChannelID = {channelID}
+	relChannelNodeQuery = `MATCH (n:Node),(c:Channel)
+	WHERE n.PubKey = {pub} AND c.ChannelID = {channelID}
 	CREATE (n)-[r:OPENED {
-		Node1TimeLockDelta: {node1TimeLockDelta},
-		Node1MinHtlc: {node1MinHtlc},
-		Node1FeeBaseMsat: {node1FeeBaseMsat},
-		Node1FeeRateMilliMsat: {node1FeeRateMilliMsat},
-		Node1Disabled: {node1Disabled}
-	} ]->(c)`
-
-	relChannelNode2Query = `MATCH (n:Node),(c:Channel)
-	WHERE n.PubKey = {node2Pub} AND c.ChannelID = {channelID}
-	CREATE (n)-[r:OPENED {
-		Node2TimeLockDelta: {node2TimeLockDelta},
-		Node2MinHtlc: {node2MinHtlc},
-		Node2FeeBaseMsat: {node2FeeBaseMsat},
-		Node2FeeRateMilliMsat: {node2FeeRateMilliMsat},
-		Node2Disabled: {node2Disabled}
+		TimeLockDelta: {timeLockDelta},
+		MinHtlc: {minHtlc},
+		FeeBaseMsat: {feeBaseMsat},
+		FeeRateMilliMsat: {feeRateMilliMsat},
+		Disabled: {disabled}
 	} ]->(c)`
 )
 
@@ -64,28 +54,28 @@ func (ci ChannelsImporter) Import(channels []ln.Channel, counter chan int) error
 		}
 
 		node1Values := map[string]interface{}{
-			"channelID":             c.ChannelID,
-			"node1Pub":              c.Node1Pub,
-			"node1TimeLockDelta":    c.Node1Policy.TimeLockDelta,
-			"node1MinHtlc":          c.Node1Policy.MinHtlc,
-			"node1FeeBaseMsat":      c.Node1Policy.FeeBaseMsat,
-			"node1FeeRateMilliMsat": c.Node1Policy.FeeRateMilliMsat,
-			"node1Disabled":         c.Node1Policy.Disabled,
+			"channelID":        c.ChannelID,
+			"pub":              c.Node1Pub,
+			"timeLockDelta":    c.Node1Policy.TimeLockDelta,
+			"minHtlc":          c.Node1Policy.MinHtlc,
+			"feeBaseMsat":      c.Node1Policy.FeeBaseMsat,
+			"feeRateMilliMsat": c.Node1Policy.FeeRateMilliMsat,
+			"disabled":         c.Node1Policy.Disabled,
 		}
 
 		node2Values := map[string]interface{}{
-			"channelID":             c.ChannelID,
-			"node2Pub":              c.Node2Pub,
-			"node2TimeLockDelta":    c.Node2Policy.TimeLockDelta,
-			"node2MinHtlc":          c.Node2Policy.MinHtlc,
-			"node2FeeBaseMsat":      c.Node2Policy.FeeBaseMsat,
-			"node2FeeRateMilliMsat": c.Node2Policy.FeeRateMilliMsat,
-			"node2Disabled":         c.Node2Policy.Disabled,
+			"channelID":        c.ChannelID,
+			"pub":              c.Node2Pub,
+			"timeLockDelta":    c.Node2Policy.TimeLockDelta,
+			"minHtlc":          c.Node2Policy.MinHtlc,
+			"feeBaseMsat":      c.Node2Policy.FeeBaseMsat,
+			"feeRateMilliMsat": c.Node2Policy.FeeRateMilliMsat,
+			"disabled":         c.Node2Policy.Disabled,
 		}
 
 		if _, err := ci.conn.ExecPipeline([]string{
-			relChannelNode1Query,
-			relChannelNode2Query,
+			relChannelNodeQuery,
+			relChannelNodeQuery,
 		}, node1Values, node2Values); err != nil {
 			return err
 		}
