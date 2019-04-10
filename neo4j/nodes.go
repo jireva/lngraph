9b03663ceff8986a5bb26a8da28ee48e921d60d1
Xavi Soler <xavisolba@gmail.com>
2019-04-10 17:02:09 +0200

    add node addresses

--- neo4j/nodes.go
+++ neo4j/nodes.go
@@ -1,6 +1,7 @@
 package neo4j
 
 import (
+	"fmt"
 	"time"
 
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
@@ -12,7 +13,8 @@ const (
 		Alias: {alias},
 		PubKey: {pubKey},
 		LastUpdate: {lastUpdate},
-		Color: {color}
+		Color: {color},
+		Addresses: {addresses}
 	} )`
 )
 
@@ -31,11 +33,19 @@ func NewNodesImporter(conn bolt.Conn) NodesImporter {
 // Import gets multiple node resources and imports them into Neo4j.
 func (ni NodesImporter) Import(nodes []ln.Node, counter chan int) error {
 	for i, lnode := range nodes {
+		var addresses string
+		for _, a := range lnode.Addresses {
+			if addresses != "" {
+				addresses += " "
+			}
+			addresses += fmt.Sprintf("%s:%s", a.Network, a.Addr)
+		}
 		if _, err := ni.conn.ExecNeo(createLightningNodeQuery, map[string]interface{}{
 			"alias":      lnode.Alias,
 			"pubKey":     lnode.PubKey,
 			"lastUpdate": time.Unix(lnode.LastUpdate, 0).Format("2006-01-02 03:04"),
 			"color":      lnode.Color,
+			"addresses":  addresses,
 		}); err != nil {
 			return err
 		}
